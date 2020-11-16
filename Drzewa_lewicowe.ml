5c5027e83f003517232565e3403ed0ea03f6c683
kuba422 <64072253+kuba422@users.noreply.github.com>
2020-11-16 11:49:51 +0100

    Create Drzewa_lewicowe.ml

---
+++ Drzewa_lewicowe.ml
@@ -0,0 +1,50 @@
+(* autor:   Jakub Jagiella  *)
+(* projekt: Drzewa lewicowe *)
+
+(* priorytet, prawa wysokosc, lewe poddrzewo, prawe poddrzewo *)
+type 'a queue = 
+  | Null
+  | Node of 'a * int * 'a queue * 'a queue
+
+(* funkcja pomocnicza *)
+let height (q : 'a queue) = 
+  match q with
+  | Null -> 0
+  | Node(_, h, _, _) -> h
+
+
+let empty = Null
+
+exception Empty
+
+let is_empty (q : 'a queue) = 
+  (q = Null)
+
+let rec join (q1 : 'a queue) (q2 : 'a queue) =
+  match (q1, q2) with
+  | (Null, q) -> q
+  | (q, Null) -> q
+  | (Node(p1, _, l1, r1), Node(p2, _, _, _)) ->
+    if (p1 > p2) then 
+      join q2 q1 
+    else
+      let q3 = join r1 q2
+      in
+      if (is_empty q3) then
+        Node(p1, 1, l1, Null)
+      else if (is_empty l1) then
+        Node(p1, 1, q3, Null)
+      else if (height q3 > height l1) then
+        Node(p1, height l1 + 1, q3, l1)
+      else
+        Node(p1, height q3 + 1, l1, q3)
+
+let add (element : 'a) (q : 'a queue) = 
+  let q_element = Node(element, 0, Null, Null)
+  in
+  join q q_element
+
+let delete_min (q : 'a queue) =
+  match q with 
+  | Null -> raise Empty
+  | Node(p, _, l, r) -> (p, join l r)
